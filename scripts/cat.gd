ab838e5b465d550166c2c46cc5aab4c6cb98f3c4
Zester Quinn Albano <zesterquinn.albano@gmail.com>
2024-06-26 19:34:23 +0800

    add animation,tilemap collision

--- scripts/cat.gd
+++ scripts/cat.gd
@@ -3,14 +3,14 @@ extends CharacterBody2D
 
 @onready var animation = $AnimatedSprite2D
 
-const SPEED = 300.0
+const SPEED = 150.0
 const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
 func _physics_process(delta):
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -22,9 +22,17 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
+	
 	if direction:
 		velocity.x = direction * SPEED
+		animation.play("run")
+		
+		if(direction < 0):
+			animation.flip_h = true
+		elif (direction > 0):
+			animation.flip_h = false
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		animation.play("idle")
 
 	move_and_slide()

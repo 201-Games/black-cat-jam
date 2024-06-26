012f2ca6f6fb95079da4b574c1a5a25af326bf5d
Zester Quinn Albano <zesterquinn.albano@gmail.com>
2024-06-26 23:30:34 +0800

    fix animations

--- scripts/cat.gd
+++ scripts/cat.gd
@@ -14,25 +14,36 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		
+	if velocity.y > 0:
+		animation.play('fall')
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+		animation.play('jump')
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
+		
+	if velocity.x == 0 and velocity.y == 0 and is_on_floor():
+		animation.play("idle")
 	
+	if(direction < 0):
+		animation.flip_h = true
+	elif (direction > 0):
+		animation.flip_h = false
+		
 	if direction:
 		velocity.x = direction * SPEED
-		animation.play("run")
-		
-		if(direction < 0):
-			animation.flip_h = true
-		elif (direction > 0):
-			animation.flip_h = false
+		if is_on_floor():
+			animation.play("run")
+			
+		if(velocity.y < 0):
+			animation.play("jump")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		animation.play("idle")
+	
 
 	move_and_slide()
